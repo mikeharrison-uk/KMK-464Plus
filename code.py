0eef34d8f97db6965a930795a563c3242a2b7bc6
onyerbikemike <75813273+mikeharrison-uk@users.noreply.github.com>
2025-02-13 17:57:44 +0000

    Updated shift layer to include all special characters

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+#AMSTRAD 464 PLUS firmware - Mike Harrison 2025
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC, make_key
 from kmk.scanners import DiodeOrientation
@@ -12,6 +13,8 @@ layers = Layers()
 macros = Macros()
 keyboard.modules.append(macros)
 keyboard.debug_enabled = False
+keyboard.modules.append(layers)
+keyboard.extensions.append(International())
 
 #Keys row and column pinout
 keyboard.col_pins = (board.GP16, board.GP17, board.GP18, board.GP19, board.GP20, board.GP21, board.GP22, board.GP26, board.GP27, board.GP28)
@@ -21,34 +24,59 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 # Define the macros for non standard keys
 AT_SYMBOL = KC.MACRO('"')
 GBP_SYMBOL = KC.MACRO('#')
-BSL_SYMBOL = KC.MACRO('~')
+HASH_SYMBOL = KC.MACRO('\\')
 
-#Keymap
 keyboard.keymap = [
-    [KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.DEL, KC.NO, KC.NO,
-     KC.N1, KC.N2, KC.ESC, KC.Q, KC.TAB, KC.A, KC.CAPS, KC.Z, KC.NO, KC.NO,
-     KC.N4, KC.N3, KC.E, KC.W, KC.S, KC.D, KC.C, KC.X, KC.NO, KC.NO,
-     KC.N6, KC.N5, KC.R, KC.T, KC.G, KC.F, KC.B, KC.V, KC.NO, KC.NO,
-     KC.N8, KC.N7, KC.U, KC.Y, KC.H, KC.J, KC.N, KC.SPACE, KC.NO, KC.NO,
-     KC.N0, KC.N9, KC.O, KC.I, KC.L, KC.K, KC.M, KC.COMMA, KC.NO, KC.NO,
-     GBP_SYMBOL, KC.EQUAL, AT_SYMBOL, KC.P, KC.SCLN, KC.COLON, KC.SLSH, KC.DOT, KC.NO, KC.NO,
-     KC.BSPC, KC.LBRC, KC.ENT, KC.RBRC, KC.F4, KC.LSFT, KC.PIPE, KC.LCTL, KC.NO, KC.NO,
-     KC.LEFT, KC.LGUI, KC.F7, KC.F8, KC.F5, KC.F1, KC.F2, KC.MO(1), KC.NO, KC.NO,
-     KC.UP, KC.RIGHT, KC.DOWN, KC.F9, KC.F6, KC.F3, KC.PENT, KC.PDOT, KC.NO, KC.NO],
 
-     #Custom layer (when you press f0 key)
-     [KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.DEL, KC.NO, KC.NO,
-     KC.EXCLAIM, KC.DOUBLE_QUOTE, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.DOLLAR, KC.NONUS_HASH, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.AMPERSAND, KC.PERCENT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.LPRN, KC.QUOTE, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.UNDERSCORE, KC.RPRN, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.CARET, KC.MINS, KC.PIPE, KC.PLUS, KC.NO, KC.ASTERISK, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-     KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO],
+    # AMSTRAD 464 Plus QWERTY (Layer 0)
+    # ,-------------------------------------------------------------------------------------------------------------------------------.
+    # |  ESC   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   -  |   £  |  CLR |  DEL |  f7  |  f8  |  f9  |
+    # |--------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+    # |  TAB   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   @  |   [  |   RETURN    |  f4  |  f5  |  f6  |
+    # |--------+------+------+------+------+-------------+------+------+------+------+------+------+             +------+------+------|
+    # |  CAPS  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   :  |   ;  |   ]  |             |  f1  |  f2  |  f3  |
+    # |--------+------+------+------+------+------+------+------+------+------+------+------+------+-------------+------+------+------|
+    # |  SHIFT |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   \  | #####|    SHIFT    |  f0  |  UP  |   .  |
+    # |--------+------+------+------+------+------+------+------+------+------+------+------+------+-------------+------+------+------|
+    # | CONTROL| COPY |                               SPACE                                        |    ENTER    | LEFT | DOWN | RIGHT|
+    # `--------------------------------------------------------------------------------------------+-------------+------+------+------'
+
+    [KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.DEL,
+     KC.NO, KC.N1, KC.N2, KC.ESC, KC.Q, KC.TAB, KC.A, KC.CAPS, KC.Z, KC.NO,
+     KC.NO, KC.N4, KC.N3, KC.E, KC.W, KC.S, KC.D, KC.C, KC.X, KC.NO,
+     KC.NO, KC.N6, KC.N5, KC.R, KC.T, KC.G, KC.F, KC.B, KC.V, KC.NO,
+     KC.NO, KC.N8, KC.N7, KC.U, KC.Y, KC.H, KC.J, KC.N, KC.SPACE, KC.NO,
+     KC.NO, KC.N0, KC.N9, KC.O, KC.I, KC.L, KC.K, KC.M, KC.COMMA, KC.NO,
+     KC.NO, KC.LSFT(KC.N6), KC.MINUS, AT_SYMBOL, KC.P, KC.SCLN, KC.COLON, KC.SLSH, KC.DOT, KC.NO,
+     KC.NO, KC.BSPC, KC.LBRC, KC.ENT, KC.RBRC, KC.F4, KC.MO(1), KC.NONUS_BSLASH, KC.LCTL, KC.NO,
+     KC.NO, KC.LEFT, KC.LCTL(KC.C), KC.F7, KC.F8, KC.F5, KC.F1, KC.F2, KC.F10, KC.NO,
+     KC.NO, KC.UP, KC.RIGHT, KC.DOWN, KC.F9, KC.F6, KC.F3, KC.PENT, KC.PDOT, KC.NO],
+    
+
+    # AMSTRAD 464 Plus SHIFTED enabling non-standard special character mappings (Layer 1)
+    # ,-------------------------------------------------------------------------------------------------------------------------------.
+    # |        |   !  |   "  |   #  |   $  |   %  |   &  |   '  |   (  |   )  |   _  |   =  |   ^  |      |      |      |  UP  |      |
+    # |--------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+    # |        |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   |  |   {  |   RETURN    | LEFT | DOWN | RIGHT|
+    # |--------+------+------+------+------+-------------+------+------+------+------+------+------+             +------+------+------|
+    # |        |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   *  |   +  |   }  |             |      |      |      |
+    # |--------+------+------+------+------+------+------+------+------+------+------+------+------+-------------+------+------+------|
+    # |        |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   <  |   >  |   ?  |   '  |      |             |      |      |      |
+    # |--------+------+------+------+------+------+------+------+------+------+------+------+------+-------------+------+------+------|
+    # |        | GUI  |                                                                            |    ENTER    |      |      |      |
+    # `--------------------------------------------------------------------------------------------+-------------+------+------+------'
+
+    [KC.NO,KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.DEL,
+     KC.NO, KC.EXCLAIM, KC.AT, KC.NO, KC.LSFT(KC.Q), KC.NO, KC.LSFT(KC.A), KC.NO, KC.LSFT(KC.Z), KC.NO,
+     KC.NO, KC.DOLLAR, KC.NONUS_HASH, KC.LSFT(KC.E), KC.LSFT(KC.W), KC.LSFT(KC.S), KC.LSFT(KC.D), KC.LSFT(KC.C), KC.LSFT(KC.X), KC.NO,
+     KC.NO, KC.AMPERSAND, KC.PERCENT, KC.LSFT(KC.R), KC.LSFT(KC.T), KC.LSFT(KC.G), KC.LSFT(KC.F), KC.LSFT(KC.B), KC.LSFT(KC.V), KC.NO,
+     KC.NO, KC.LPRN, KC.QUOTE, KC.LSFT(KC.U), KC.LSFT(KC.Y), KC.LSFT(KC.H), KC.LSFT(KC.J), KC.LSFT(KC.N), KC.NO, KC.NO,
+     KC.NO, KC.UNDERSCORE, KC.RPRN, KC.LSFT(KC.O), KC.LSFT(KC.I), KC.LSFT(KC.L), KC.LSFT(KC.K), KC.LSFT(KC.M), KC.LSFT(KC.COMMA), KC.NO,
+     KC.NO, GBP_SYMBOL, KC.EQUAL, KC.LSFT(KC.NONUS_BSLASH), KC.LSFT(KC.P), KC.LSFT(KC.EQUAL), KC.LSFT(KC.N8), KC.LSFT(KC.SLSH), KC.LSFT(KC.DOT), KC.NO,
+     KC.NO, KC.NO, KC.LSFT(KC.LBRC), KC.LSFT(KC.ENT), KC.LSFT(KC.RBRC), KC.LEFT, KC.NO, KC.GRAVE, KC.NO, KC.NO,
+     KC.NO, KC.NO, KC.LGUI, KC.NO, KC.UP, KC.DOWN, KC.NO, KC.NO, KC.NO, KC.NO,
+     KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.RIGHT, KC.NO, KC.NO, KC.NO, KC.NO],
 ]
 
 if __name__ == '__main__':
     keyboard.go()
-    
